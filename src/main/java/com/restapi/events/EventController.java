92a8d5c517cfa5c316b3b8b546f7288e0e5eb999
Bamdule <Bamdule5@gmail.com>
2021-06-14 14:00:11 +0900

    DTO validation

--- src/main/java/com/restapi/events/EventController.java
+++ src/main/java/com/restapi/events/EventController.java
@@ -1,28 +1,28 @@
 package com.restapi.events;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.net.URI;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 
 @Controller
-@RequestMapping(value = "/api/events",produces = MediaTypes.HAL_JSON_VALUE)
+@RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
 
     private final EventRepository eventRepository;
     private final ModelMapper modelMapper;
 
-    public EventController(EventRepository eventRepository ,ModelMapper mm){
+    public EventController(EventRepository eventRepository, ModelMapper mm) {
         this.modelMapper = mm;
         this.eventRepository = eventRepository;
     }
@@ -30,8 +30,12 @@ public class EventController {
 
     @PostMapping
     public ResponseEntity createEvent(
-            @RequestBody EventDto eventDto
-    ){
+            @RequestBody @Valid EventDto eventDto, Errors errors
+    ) {
+        if(errors.hasErrors()){
+            return ResponseEntity.badRequest().build();
+        }
+
         Event event = modelMapper.map(eventDto, Event.class);
 
         Event newEvent = this.eventRepository.save(event);
